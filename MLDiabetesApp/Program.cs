97eff4b7151cf1332f955b883e66d71bb3e083ef
CngzKrs <ycakarsu@gmail.com>
2019-12-24 17:20:13 +0300

    Add files via upload

---
+++ MLDiabetesApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using MLDiabetesAppML.Model;
+using System;
+
+namespace MLDiabetesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            var input = new ModelInput();
+            input.Pregnancies = 6;
+            input.Glucose = 50;
+            input.BloodPressure = 70;
+            input.SkinThickness = 25;
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"[0: Not Diabet, 1: Diabet] - The prediction result: {result.Prediction}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Microsoft.ML.ModelBuilder
+This all documents, notes and all codes inspired from https://dotnet.microsoft.com/learn/ml-dotnet/get-started-tutorial/intro
+I developed the codes step by step following the course.
+
+"Install the ML.NET Model Builder extension for Visual Studio, then train and use your first machine learning model with ML.NET."  
+  
+For dataset, I used "Pima Indians Diabetes Database" from Kaggle: https://www.kaggle.com/uciml/pima-indians-diabetes-database
